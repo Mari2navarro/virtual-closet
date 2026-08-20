8c2751d53899348cbbf06b59e6a4224f41e030fc
Mari <navarro.ortiz.maricarmen@gmail.com>
2026-08-20 12:40:51 +0200

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main>
+      <h1>Armario Virtual</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data/clothes.js
@@ -0,0 +1,90 @@
+{
+  id: 1,
+  name: "Camisa blanca",
+  category: "top",
+  color: "white",
+  style: ["minimal", "elegant", "casual", "urban"],
+  season: "all",
+  occasion: ["work", "casual", "dinner"],
+  formality: 2,
+  image: "/images/white-shirt.jpg",
+  favorite: false
+}
+
+{
+  id: 2,
+  name: "Top negro básico",
+  category: "top",
+  color: "black",
+  style: ["casual", "minimal", "urban"],
+  season: "all",
+  occasion: ["work", "casual", "dinner", "date", "event", "party"],
+  formality: 1,
+  image: "/images/black-basic-top.jpg",
+  favorite: false
+}
+
+{
+  id: 2,
+  name: "Pantalones Vaqueros",
+  category: "bottom",
+  color: "blue",
+  style: ["casual", "minimal", "urban", "romantic"],
+  season: "all",
+  occasion: ["work", "casual", "dinner", "date", "event", "party"],
+  formality: 2,
+  image: "/images/blue-jeans.jpg",
+  favorite: false
+}
+
+{
+  id: 4,
+  name: "Pantalones de traje negros",
+  category: "bottom",
+  color: "black",
+  style: ["minimal", "elegant", "romantic"],
+  season: "all",
+  occasion: ["work", "casual", "dinner", "date", "event", "party"],
+  formality: 4, 
+  image: "/images/black-tailored-trousers.jpg",
+  favorite: false
+}
+
+{
+  id: 5,
+  name: "Falda midi marrón",
+  category: "bottom",
+  color: "brown",
+  style: ["casual", "minimal", "urban", "elegant", "urban", "romantic"],
+  season: "all",
+  occasion: ["work", "casual", "dinner", "date", "event"],
+  formality: 4,
+  image: "/images/brown-midi-skirt.jpg",
+  favorite: false
+}
+
+{
+  id: 6,
+  name: "Vestido negro",
+  category: "dress",
+  color: "black",
+  style: ["casual", "minimal", "urban", "romantic"],
+  season: "all",
+  occasion: ["work", "casual", "dinner", "date", "event", "party"],
+  formality: 4,
+  image: "/images/black-dress.jpg",
+  favorite: false
+}
+
+{
+  id: 7,
+  name: "Top negro básico",
+  category: "top",
+  color: "black",
+  style: ["casual", "minimal", "urban"],
+  season: "all",
+  occasion: ["work", "casual", "dinner", "date", "event"],
+  formality: 1,
+  image: "/images/black-basic-top.jpg",
+  favorite: false
+}
