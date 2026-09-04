0ddd4335c98e83cfbf8b43cd57560df3f5b08362
Mari <navarro.ortiz.maricarmen@gmail.com>
2026-09-04 18:47:27 +0200

    Add empty wardrobe state

--- src/components/clothing_grid/clothing-grid.jsx
+++ src/components/clothing_grid/clothing-grid.jsx
@@ -1,13 +1,16 @@
 import ClothingCard from "../clothing_card/clothing-card";
 import "./clothing-grid.scss";
 
+import { SearchX } from "lucide-react";
+
 function ClothingGrid({ clothes, onToggleFavorite }) {
   if (clothes.length === 0) {
     return (
       <section className="clothing-grid clothing-grid--empty">
-        <p className="clothing-grid__empty-message">
-          No hemos encontrado ninguna prenda 🥲
-        </p>
+        <div className="clothing-grid__empty-message">
+  <SearchX className="clothing-grid__empty-icon" />
+  <p>No hemos encontrado ninguna prenda</p>
+</div>
       </section>
     );
   }
